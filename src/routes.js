fbcf890ae6fbd88276a1ff83652565a50e8806b8
Luan Azevedo <azeveedolh@gmail.com>
2019-07-06 10:08:45 -0300

    Organization

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 import { Router } from 'express';
-
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import authMiddleware from './app/middlewares/auth';
 
 const routes = Router();
 
